b6136f988a81440e15f42aaa0ba8c7c2410f506f
Pavel1367 <pashka1367@bk.ru>
2022-12-28 21:57:49 +0300

    1

---
+++ src/components/UI/loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './loader.module.css'
+function Loader() {
+  return (
+    <div className={classes.loader}>
+      
+    </div>
+  )
+}
+
+export default Loader
---
+++ src/components/UI/modal/Modal.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Modal.module.css";
+export default function Modal({ children, visible, setVisible }) {
+
+  const rootClasses = [classes.myModal]
+  if(visible){
+    rootClasses.push(classes.active)
+  }
+  return (
+    <div onClick={() => setVisible(false)}className={rootClasses.join(" ")}>
+      <div onClick={(e) => e.stopPropagation()} className={classes.myModalContent}>{children}</div>
+    </div>
+  );
+}
